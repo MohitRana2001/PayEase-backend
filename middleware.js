df9bd83d122fb765c8bc1106dcd9d49335c4ec35
MOHIT RANA <awesomemohit20@gmail.com>
2024-10-08 15:36:46 +0530

    Revert "fetch issue"
    This reverts commit 2b4515d8c98dc48612d4c740ef25f7443f060592.
    Unwanted changes

--- middleware.js
+++ middleware.js
@@ -1,29 +1,27 @@
 // middleware.js
-import fetch from 'node-fetch'; 
-
 export default function middleware(req, res, next) {
-  const token = req.headers.authorization?.replace('Bearer ', '');
-
-  if (token) {
-    fetch('/api/auth/verifyToken', { 
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ token })
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.valid) {
-        req.userId = data.user.userId; 
-        next();
-      } else {
-        res.status(401).json({ message: 'Unauthorized' });
-      }
-    })
-    .catch(error => {
-      console.error('Error verifying token:', error);
-      res.status(500).json({ message: 'Internal server error' });
-    });
-  } else {
-    next(); 
+    const token = req.headers.authorization?.replace('Bearer ', '');
+  
+    if (token) {
+      fetch('/api/auth/verifyToken', { 
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ token })
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.valid) {
+          req.userId = data.user.userId; 
+          next();
+        } else {
+          res.status(401).json({ message: 'Unauthorized' });
+        }
+      })
+      .catch(error => {
+        console.error('Error verifying token:', error);
+        res.status(500).json({ message: 'Internal server error' });
+      });
+    } else {
+      next(); 
+    }
   }
-}
