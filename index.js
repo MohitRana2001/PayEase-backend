2c58f555cef70d13b5136ffbf665a488f2fd7c70
MOHIT RANA <awesomemohit20@gmail.com>
2024-10-08 15:37:48 +0530

    Revert "experimenting with vercel"
    This reverts commit b07cfac281b1dbe56a510697c470ad3a1cf4f96b.

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-// index.js
-import express from 'express';
-import cors from 'cors';
-import rootRouter from './routes/index';
+const express = require("express");
+const cors = require("cors");
+const rootRouter = require("./routes/index");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/v1', rootRouter);
+app.use("/api/v1", rootRouter);
 
-export default function handler(req, res) {
-  // Let Express handle the request
-  app(req, res);
-}
+app.listen(3000);
